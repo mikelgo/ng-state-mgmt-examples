77869404907753a2dc2cc0e3e85ebdebc29caead
mikelgo <m.gonser@icloud.com>
2020-09-26 20:40:45 +0200

    ngxs: register state in module

--- apps/ngxs/src/app/app.module.ts
+++ apps/ngxs/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { environment } from '../environments/environment';
 import { SharedModule } from '@ng-state-mgmt-examples/shared';
+import { TodoStateService } from './store/todo-state.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    NgxsModule.forRoot([], {
+    NgxsModule.forRoot([TodoStateService], {
       developmentMode: !environment.production
     }),
     SharedModule
