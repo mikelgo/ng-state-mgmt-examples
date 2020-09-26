95852d50421f3a3adda6a0b69aa1d9219f2e366b
mikelgo <m.gonser@icloud.com>
2020-09-26 20:41:02 +0200

    ngxs: implement example

--- apps/ngxs/src/app/app.component.ts
+++ apps/ngxs/src/app/app.component.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { BaseCrudComponent } from '@ng-state-mgmt-examples/shared';
 import { StateService } from './state.service';
 
-
 @Component({
   selector: 'ng-state-mgmt-examples-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent extends BaseCrudComponent implements OnInit{
+export class AppComponent extends BaseCrudComponent implements OnInit {
   title = 'ngxs';
   constructor(public stateService: StateService) {
     // TODO
     super(stateService);
   }
   ngOnInit() {
-
+    // this.stateService.todos$.subscribe(console.log)
+    // this.stateService.open$.subscribe(console.log)
   }
 }
