f0a498567f220a518b29d11b51ee2c771271214b
mikelgo <m.gonser@icloud.com>
2020-09-26 13:41:59 +0200

    initial commit

---
+++ apps/ngrx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-state-mgmt-examples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngrx';
+}
