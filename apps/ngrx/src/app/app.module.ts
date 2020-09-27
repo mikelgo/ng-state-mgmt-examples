4f66cdd809b3b9025b3a68eb89fc5740e256331f
mikelgo <m.gonser@icloud.com>
2020-09-27 10:13:49 +0200

    ngrx: register effects and devtools

--- apps/ngrx/src/app/app.module.ts
+++ apps/ngrx/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppComponent } from './app.component';
 import { SharedModule } from '@ng-state-mgmt-examples/shared';
 import { StoreModule } from '@ngrx/store';
 import { reducer } from './store/todo.reducer';
+import { TodoEffects } from './store/todo-effects.service';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, StoreModule.forRoot({ todos: reducer})],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ todos: reducer }),
+    EffectsModule.forRoot([TodoEffects, ]),
+    SharedModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
